d3a5994e532a034ea09fd632b50d28000974337f
sadler2007@gmail.com <sadler2007@gmail.com>
2018-09-02 18:03:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function add(a, b){
-  return "a + b"
+  return a + b
 }
 
 function subtract (a, b){
-  return "a - b"
+  return a - b
 }
 
 function multiply(a, b){
-  return "a * b"
+  return a * b
 }
 
 function divide (a, b){
